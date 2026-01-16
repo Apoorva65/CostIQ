afba26578b900fa5af8d1d9b9046e783427de944
Apoorva65 <apoorvasdixit@gmail.com>
2026-01-16 17:28:16 +0530

    Initial setup: Vite+React for CostIQ

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>CostIq</h1>
+      <p>Personal expense management made simple.</p>
+    </>
+  )
+}
+
+export default App
